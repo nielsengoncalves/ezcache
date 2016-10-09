53084ea68cff84aa0e6d64b6484c4d5d7e774dca
Nielsen Gonçalves <ni_elsen@hotmail.com>
2016-10-09 19:41:43 -0300

    phpunit for fileCache

--- tests/FileCacheTest.php
+++ tests/FileCacheTest.php
@@ -1,23 +1,115 @@
 <?php
 
-namespace cache;
+namespace EzcacheTest\Cache;
 
-use phpDocumentor\Reflection\Types\Self_;
+use Ezcache\Cache\FileCache;
+use org\bovigo\vfs\vfsStream;
 use PHPUnit_Framework_TestCase;
+use stdClass;
 
 class FileCacheTest extends PHPUnit_Framework_TestCase {
 
-    const DIR = '%tmp%/ezcache-test/cache';
+    use PHPUnitUtilsTrait;
 
-//    public function testGetNotExistingOnCache() {
-//        $fileCache = new FileCache(self::DIR);
-//        $data = $fileCache->get('tst1');
-//        $this->assertEquals(["is_hit" => false, "value" => null], $data);
-//        rmdir(DIR);
-//    }
-//
-//    public function testGetExisting() {
-//
-//    }
+    private $file;
+    private $getFileData;
 
+    public function setUp() {
+        vfsStream::setup();
+        $this->getFileData = $this->getPrivateMethod('Ezcache\Cache\FileCache', 'getFileData');
+        $this->file = new FileCache(vfsStream::url('cache'));
+    }
+
+    public function tearDown() {
+
+    }
+
+    /**
+     * Tests setNamespace method
+     */
+    public function testSetNamespace() {
+
+        $file = new FileCache(vfsStream::url('cache'), 0, 'namespacename1');
+        $property = $this->getPrivateProperty('Ezcache\Cache\FileCache', 'namespace');
+
+        // Checks if constructor namespace is working
+        $this->assertEquals($property->getValue($file), 'namespacename1', 'Asserting that passing namespace on constructor works.');
+
+        // Checks if the setNamespace method is working
+        $file->setNamespace('namespacename2');
+        $this->assertEquals($property->getValue($file), 'namespacename2', 'Asserting that setNamespace method works.');
+    }
+
+    /**
+     * Tests if set expiration is working
+     */
+    public function testSetExpiration() {
+        $this->withExpirationTest();
+        $this->withNoExpirationTest();
+    }
+
+    /**
+     * Tests setting and getting all data types supported by PSR-6
+     */
+    public function testSetGetAllTypes() {
+        $this->typeTest('string',  'abcd');
+        $this->typeTest('integer', 999999);
+        $this->typeTest('double',  12.223);
+        $this->typeTest('boolean', false);
+        $this->typeTest('null',    null);
+        $this->typeTest('array',   ['key1' => 1, 'key2' => 2]);
+        $this->typeTest('object',  new stdClass());
+    }
+
+    /**
+     * Tests deleting files from cache
+     */
+    public function testDelete() {
+        $toBeDeleted = 'FileToBeDeleted';
+        $this->file->set($toBeDeleted, 10);
+        $data = $this->file->get($toBeDeleted);
+        $this->assertEquals(10, $data, 'Asserting that file exists.');
+
+        $this->file->delete($toBeDeleted);
+        $data = $this->file->get($toBeDeleted);
+        $this->assertEquals(null, $data, 'Asserting that file was deleted.');
+    }
+
+    /**
+     * Tests no expiration cache
+     */
+    private function withNoExpirationTest() {
+        $noExpirationInt = 'NoExpiration';
+        $this->file->set($noExpirationInt, 10);
+        $fileData = $this->getFileData->invokeArgs($this->file, [$noExpirationInt]);
+        $this->assertGreaterThanOrEqual(3153600000, strtotime($fileData["expires_at"]) - strtotime($fileData["created_at"]), 'Asserting that cache expiration is bigger or equals a year with default value.');
+
+        $this->file->set($noExpirationInt, 99, 0);
+        $fileData = $this->getFileData->invokeArgs($this->file, [$noExpirationInt]);
+        $this->assertGreaterThanOrEqual(3153600000, strtotime($fileData["expires_at"]) - strtotime($fileData["created_at"]), 'Asserting that cache expiration is bigger or equals a year forcing no expiration.');
+    }
+
+    /**
+     * Tests cache with expiration time
+     */
+    private function withExpirationTest() {
+        $oneMinuteExp = 'OneMinuteExpiration';
+        $this->file->set($oneMinuteExp, 'val1', 60);
+        $fileData = $this->getFileData->invokeArgs($this->file, [$oneMinuteExp]);
+        $this->assertEquals(60, strtotime($fileData["expires_at"]) - strtotime($fileData["created_at"]), 'Asserting that cache expiration is one minute.');
+    }
+
+    /**
+     * Sets a value to cache, and verifies if the get method gets the correct type
+     *
+     * @param string $type the type being checked
+     * @param mixed $value the value of given type
+     */
+    private function typeTest(string $type, $value) {
+        $fileName = ucfirst($type) . 'Type';
+        $this->file->set($fileName, $value);
+        $data = $this->file->get($fileName);
+        $this->assertEquals($value, $data);
+        $this->assertInternalType($type, $data, 'Asserting that ' . $type . ' was stored correctly.');
+    }
 }
--- tests/PHPUnitUtilsTrait.php
+++ tests/PHPUnitUtilsTrait.php
@@ -1,19 +1,38 @@
 <?php
 
-class PHPUnitUtils extends {
+namespace EzcacheTest\Cache;
+
+use ReflectionClass;
+use ReflectionMethod;
+use ReflectionProperty;
+
+trait PHPUnitUtilsTrait {
 
     /**
-     * getPrivateProperty
+     * Get private property from a class
      *
-     * @param 	string $className
-     * @param 	string $propertyName
+     * @param 	string $className the class name
+     * @param 	string $propertyName the property name
      *
      * @return	ReflectionProperty
      */
     public function getPrivateProperty($className, $propertyName) : ReflectionProperty {
-        $reflector = new ReflectionClass($className);
-        $property = $reflector->getProperty($propertyName);
+        $property = (new ReflectionClass($className))->getProperty($propertyName);
         $property->setAccessible(true);
         return $property;
+    }
+
+    /**
+     * Get private method from a class
+     *
+     * @param 	string $className the class name
+     * @param 	string $methodName the method name
+     *
+     * @return	ReflectionMethod
+     */
+    public function getPrivateMethod($className, $methodName) : ReflectionMethod {
+        $method = (new ReflectionClass($className))->getMethod($methodName);
+        $method->setAccessible(true);
+        return $method;
     }
 }
